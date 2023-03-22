3d6e34a45bb3137b969a10da49ff78523f3e9b34
Alberto JR <albertojunquera.ra@gmail.com>
2023-03-22 10:20:40 +0100

    Initial Commit

---
+++ PracticaIOSSuperPoderes/PracticaIOSSuperPoderesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticaIOSSuperPoderesApp.swift
+//  PracticaIOSSuperPoderes
+//
+//  Created by Alberto Junquera Ramírez on 22/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticaIOSSuperPoderesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
