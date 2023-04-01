843abd1d43992fed61f28fd56de351a0d0a71cf8
Alberto JR <albertojunquera.ra@gmail.com>
2023-04-01 19:18:21 +0200

    CleanCode: All the models

--- PracticaIOSSuperPoderes/Models/Heroe.swift
+++ PracticaIOSSuperPoderes/Models/Heroe.swift
@@ -26,72 +26,16 @@ struct DataClass: Codable{
     let count: Int
     let results: [Heroe]
 }
- //MARK: - Heroe -
-//struct Heroe: Codable, Identifiable{
-//    let id: Int
-//    let name: String
-//    let description: String
-//    let modified: String
-//    //let modified: Date //TODO: With date, it gives an Error, with String it works perfectly
-//    let thumbnail: Thumbnail
-//    let resourceURI: String
-//    let comics: Comics
-//    let series: Comics
-//    let stories: Stories
-//    let events: Comics
-//    let urls: [URLElement]
-//}
+ 
 //MARK: - Heroe -
 struct Heroe: Codable, Identifiable{
    let id: Int
    let name: String
    let description: String
-  // let modified: String
-   //let modified: Date //TODO: With date, it gives an Error, with String it works perfectly
    let thumbnail: Thumbnail
-//   let resourceURI: String
-//   let comics: Comics
-//   let series: Comics
-//   let stories: Stories
-//   let events: Comics
-//   let urls: [URLElement]
-}
-// MARK: - Comics
-struct Comics: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [ComicsItem]
-    let returned: Int
 }
 
-// MARK: - ComicsItem
-struct ComicsItem: Codable {
-    let resourceURI: String
-    let name: String
-}
-
-// MARK: - Stories
-struct Stories: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [StoriesItem]
-    let returned: Int
-}
-
-// MARK: - StoriesItem
-struct StoriesItem: Codable {
-    let resourceURI: String
-    let name: String
-    let type: ItemType?
-}
-
-enum ItemType: String, Codable {
-    case cover = "cover"
-    case empty = ""
-    case interiorStory = "interiorStory"
-}
-
-// MARK: - Thumbnail
+// MARK: - Thumbnail -
 struct Thumbnail: Codable {
     let path: String
     let thumbnailExtension: Extension?
@@ -104,17 +48,5 @@ struct Thumbnail: Codable {
 
 enum Extension: String, Codable {
     case jpg = "jpg"
-}
-
-// MARK: - URLElement
-struct URLElement: Codable {
-    let type: URLType?
-    let url: String
-}
-
-enum URLType: String, Codable {
-    case comiclink = "comiclink"
-    case detail = "detail"
-    case wiki = "wiki"
 }
 
--- PracticaIOSSuperPoderes/Models/Serie.swift
+++ PracticaIOSSuperPoderes/Models/Serie.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-
-// MARK: - SeriesResult
+// MARK: - SeriesResult -
 struct SeriesResult: Codable {
     let code: Int
     let status, copyright, attributionText, attributionHTML: String
@@ -16,138 +15,16 @@ struct SeriesResult: Codable {
     let data: DataClassSeries
 }
 
-// MARK: - DataClass
+// MARK: - DataClass -
 struct DataClassSeries: Codable {
     let offset, limit, total, count: Int
     let results: [Serie]
 }
 
-//// MARK: - Result
-//struct Serie: Codable, Identifiable {
-//    let id: Int
-//    let title: String
-//    let description: String?
-//    let resourceURI: String?
-//    let urls: [URLElement]?
-//    let startYear, endYear: Int?
-//    let rating, type: String?
-//    let modified: String?
-//    let thumbnail: Thumbnail?
-//    let creators: Creators?
-//    let characters: Characters?
-//    let stories: Stories?
-//    let comics, events: Characters?
-//    let next, previous: JSONNull?
-//}
-// MARK: - Result
+// MARK: - Serie -
 struct Serie: Codable, Identifiable {
     let id: Int
     let title: String
     let description: String?
-  //  let resourceURI: String?
-  //  let urls: [URLElement]?
-  //  let startYear, endYear: Int?
-  //  let rating, type: String?
-  //  let modified: String?
     let thumbnail: Thumbnail
-   // let creators: Creators?
-  //  let characters: Characters?
-   // let stories: Stories?
-   // let comics, events: Characters?
-   // let next, previous: JSONNull?
 }
-// MARK: - Characters
-struct Characters: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [CharactersItem]?
-    let returned: Int
-}
-
-// MARK: - CharactersItem
-struct CharactersItem: Codable {
-    let resourceURI: String
-    let name: String
-}
-
-// MARK: - Creators
-struct Creators: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [CreatorsItem]?
-    let returned: Int
-}
-
-// MARK: - CreatorsItem
-struct CreatorsItem: Codable {
-    let resourceURI: String
-    let name, role: String
-}
-/*
-// MARK: - Stories
-struct Stories: Codable {
-    let available: Int
-    let collectionURI: String
-    let items: [StoriesItem]
-    let returned: Int
-}
-
-// MARK: - StoriesItem
-struct StoriesItem: Codable {
-    let resourceURI: String
-    let name: String
-    let type: TypeEnum
-}
-
-enum TypeEnum: String, Codable {
-    case cover = "cover"
-    case interiorStory = "interiorStory"
-}
- */
-
-// MARK: - Thumbnail
-struct ThumbnailSerie: Codable {
-    let path: String
-    let thumbnailExtension: String
-
-    enum CodingKeys: String, CodingKey {
-        case path
-        case thumbnailExtension = "extension"
-    }
-}
-
-/*
-// MARK: - URLElement
-struct URLElement: Codable {
-    let type: String
-    let url: String
-}
-*/
-// MARK: - Encode/decode helpers
-
-class JSONNull: Codable, Hashable {
-
-    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
-        return true
-    }
-
-    public var hashValue: Int {
-        return 0
-    }
-
-    public init() {}
-
-    public required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if !container.decodeNil() {
-            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
-        }
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encodeNil()
-    }
- 
-}
- 
