925408ddc2b3439b5fe26889b6841dc216ff1f54
Alberto JR <albertojunquera.ra@gmail.com>
2023-04-08 19:41:17 +0200

    Fixed series optional in the series view model

--- PracticaIOSSuperPoderes/ViewModels/HeroeSeriesViewModel/HeroeSeriesViewModel.swift
+++ PracticaIOSSuperPoderes/ViewModels/HeroeSeriesViewModel/HeroeSeriesViewModel.swift
@@ -11,7 +11,7 @@ import Combine
 
 final class HeroeSeriesViewModel: ObservableObject{
     @Published var status: Status = .none
-    @Published var series: [Serie] = []
+    @Published var series: [Serie]? = []
     
     @Published var heroe: Heroe
     
